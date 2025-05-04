771e31d9043eb4e24cd673717bba50226dd03488
Himanshu Kumar <hk317824@gmail.com>
2025-05-04 16:21:01 +0530

    install bootstrap in react

--- src/StyleInReact.js
+++ src/StyleInReact.js
@@ -1,12 +1,14 @@
 import React from "react";
-import './Style.css'
-import style from './custom.module.css'
+import './Style.css';
+import style from './custom.module.css';
+import {Button} from 'react-bootstrap';
 function StyleInReact (){
     return(
         <div>
             <h1 className="primary-heading" > Style Type 1 in React js</h1>
             <h1 style={{color:"black", background:"yellow"}}> Style Type 2 in React js</h1>
             <h1 className={style.type3Heading}> Style Type 3 in React js</h1>
+            <Button variant="secondary"> Primary Button </Button>
         </div>
     )
 }
